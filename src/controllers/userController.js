bf10389f65dbc8412158d6e003c0ba3e64ad90e9
youredith <youredith3@gmail.com>
2022-08-18 23:01:59 +0900

    getJoin, postJoin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,12 +1,50 @@
 import User from "../models/User";
 
 
+export const getJoin = (req, res) => {
+    return res.render("user/join", { pageTitle: "Join Webster"});
+}
+export const postJoin = async (req, res) => {
+    const pageTitle = "Join Webster!";
+    const { email, password, password2, username } = req.body;
+
+    const exists = await User.exists({ $or: [{ email }, { username }] });
+    if (exists) {
+        console.log("exists");
+        return res.status(400).render("user/join", {            
+            pageTitle,
+            errorMessage: "! This email/username is already taken.",
+        });
+    }
+    if (password !== password2) {
+        return res.status(400).render("user/join", {
+            pageTitle,
+            errorMessage: "! Password confirmation does not match.",
+        });
+    }
+
+    try {
+      await User.create({
+        email,
+        password,
+        username,
+      });
+      return res.redirect("/user/login");
+    } catch (error) {
+      console.log(error);
+    }
+};
+
+
 export const account = (req, res) => {
 
 };
 
-export const login = (req, res) => {
-
+export const getLogin = (req, res) => {
+    return res.render("user/login", { pageTitle: "Login" }); 
+};
+export const postLogin = (req, res) => {
+    return res.render("user/login", { pageTitle: "Login" }); 
 };
 export const logout = (req, res) => {
 
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,18 +1,24 @@
 import express from "express";
 import {
-    getJoin, postJoin, account, login, getEdit, postEdit, getChangePassword, postChangePassword
+  getJoin,
+  postJoin,
+  account,
+  getLogin, postLogin,
+  getEdit,
+  postEdit,
+  getChangePassword,
+  postChangePassword,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-
 userRouter.route("/join").get(getJoin).post(postJoin);
-userRouter.get("/login", login);
+userRouter.route("/login").get(getLogin).post(postLogin);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter
-    .route("/edit")
-    .get(getEdit)
-    .post(postEdit);
-userRouter.route("/change_password").get(getChangePassword).post(postChangePassword);
+  .route("/change_password")
+  .get(getChangePassword)
+  .post(postChangePassword);
 userRouter.route("/:id").get(account);
 
 export default userRouter;
