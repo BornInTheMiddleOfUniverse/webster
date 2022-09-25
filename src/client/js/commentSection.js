910db87013512457e6a5998c36709b717599f3f3
youredith <youredith3@gmail.com>
2022-09-25 23:35:33 +0900

    Fake comment 1
    Created a function to upload fake comments to make them look like they being uploaded in real time without refreshing the page.

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,45 @@
+import dayjs from (days.js);
+
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 const textarea = form.querySelector("textarea");
 const btn = form.querySelector("button");
+const createdAt = document.querySelector(".comment-createdAt");
+
+const addComment = (text) => {
+  const newCommentBox = document.createElement("div");
+
+  newCommentBox.className = "comment-box";
+  const commentOwnerProfilePic = document.createElement("div");
+  commentOwnerProfilePic.className = "comment-ownerProfilePic";
+  const commentBoxTop = document.createElement("div");
+  commentBoxTop.className = "comment-box__top";
+  const commentText = document.createElement("p");
+  commentText.className = "comment-text";
+  commentText.innerText = text;
+  const commentBoxBottom = document.createElement("div");
+  commentBoxBottom.className = "comment-box__bottom";
+
+  const likeBtns = document.createElement("div");
+  likeBtns.className = "like-btns";
+  const likeIcon = document.createElement("i");
+  likeIcon.className = "far.fa-thumbs-up";
+  const dislikeIcon = document.createElement("i");
+  dislikeIcon.className = "far.fa-thumbs-down";
+  likeBtns.appendChild(likeIcon);
+  likeBtns.appendChild(dislikeIcon);
+  const reply = document.createElement("span");
+  reply.className = "reply";
+  reply.innerText = "reply";
+  commentBoxBottom.appendChild(likeBtns);
+  commentBoxBottom.appendChild(reply);
+
+  newCommentBox.appendChild(commentOwnerProfilePic);
+  newCommentBox.appendChild(commentBoxTop);
+  newCommentBox.appendChild(commentText);
+  newCommentBox.appendChild(commentBoxBottom);
+}
+
 
 const handleSubmit = async (event) => {
   event.preventDefault();
@@ -10,7 +48,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -18,7 +56,9 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   })
   textarea.value = "";
-  window.location.reload();
+  if ( status == 201 ) {
+    addComment(text);
+  }
 };
 
 if (form) {
