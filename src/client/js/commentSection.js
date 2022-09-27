7166956c2350f41604e26e345357f9fe2a71339e
youredith <youredith3@gmail.com>
2022-09-27 23:21:39 +0900

    Fake comment2

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,5 +1,3 @@
-import dayjs from (days.js);
-
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 const textarea = form.querySelector("textarea");
@@ -7,19 +5,34 @@ const btn = form.querySelector("button");
 const createdAt = document.querySelector(".comment-createdAt");
 
 const addComment = (text) => {
+  const videoComments = document.querySelector(".comments");
   const newCommentBox = document.createElement("div");
-
   newCommentBox.className = "comment-box";
+
   const commentOwnerProfilePic = document.createElement("div");
-  commentOwnerProfilePic.className = "comment-ownerProfilePic";
+  commentOwnerProfilePic.className = "comment-owner-img";
+  
+  const ownerImg = document.getElementById("user-img");
+  console.log();
+  commentOwnerProfilePic.appendChild(ownerImg);  
+
   const commentBoxTop = document.createElement("div");
   commentBoxTop.className = "comment-box__top";
+  //need trim
+  // const ownerName = document.querySelector("brief_user_info").text();
+  // ownerName.className = "comment-owner";
+  const createdAt = document.createElement("div");
+  createdAt.className = "comment-createdAt"; 
+  createdAt.innerText = "a few seconds ago";
+  // commentBoxTop.appendChild(ownerName);
+  commentBoxTop.appendChild(createdAt);
+
   const commentText = document.createElement("p");
   commentText.className = "comment-text";
-  commentText.innerText = text;
+  commentText.innerText = `${text}`;
+
   const commentBoxBottom = document.createElement("div");
   commentBoxBottom.className = "comment-box__bottom";
-
   const likeBtns = document.createElement("div");
   likeBtns.className = "like-btns";
   const likeIcon = document.createElement("i");
@@ -37,7 +50,8 @@ const addComment = (text) => {
   newCommentBox.appendChild(commentOwnerProfilePic);
   newCommentBox.appendChild(commentBoxTop);
   newCommentBox.appendChild(commentText);
-  newCommentBox.appendChild(commentBoxBottom);
+  
+  videoComments.prepend(newCommentBox);
 }
 
 
@@ -48,7 +62,7 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -56,7 +70,7 @@ const handleSubmit = async (event) => {
     body: JSON.stringify({ text }),
   })
   textarea.value = "";
-  if ( status == 201 ) {
+  if ( response.status == 201 ) {
     addComment(text);
   }
 };
